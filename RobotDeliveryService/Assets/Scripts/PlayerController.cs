94d83e9e26dfd11d47f908149beca1e80c9b3a84
Athilc <gaccount@126.com>
2019-02-16 15:28:15 -0800

    gliding, not landing

--- RobotDeliveryService/Assets/Scripts/PlayerController.cs
+++ RobotDeliveryService/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody player;
+    private Rigidbody player;
 
 
     //for walking
@@ -14,38 +14,71 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float rotateSpeed;
     public float accelration;
-    private float currentSpeed;
-    private bool onGround=true;
+    public float fallingSpeed;
+
+    public bool onGround=true;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GetComponent<Rigidbody>();
 
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        Vector3 velocity; // on ground
+        Vector3 velocity2d; // flying planar velocity
+        Vector3 velocityVertical; // falling velocity
         if (!onGround)//gliding
         {
-            
-            //TODO
-        }
-        Vector3 velocity;
-        //moving forward and backward
-        velocity = new Vector3(0, 0, groundSpeed*v);       
-        velocity = transform.TransformDirection(velocity);
-        // turning
-        transform.Rotate(0, h * rotateSpeed, 0);
+            velocityVertical = new Vector3(0, -fallingSpeed, 0);
+            transform.localPosition += velocityVertical * Time.fixedDeltaTime;
+            velocity2d = new Vector3(0,  speed * v,0);
+            transform.Rotate( 0,0,-h * rotateSpeed);
+            if (v > 0)
+            {
+                velocity2d = transform.TransformDirection(velocity2d);
+                transform.localPosition += velocity2d * Time.fixedDeltaTime;
+                
+            }
 
-        transform.localPosition += velocity * Time.fixedDeltaTime;
+        }
+
+        else{
+            //moving forward and backward
+            velocity = new Vector3(0, 0, groundSpeed * v);
+            velocity = transform.TransformDirection(velocity);
+            // turning
+            transform.Rotate(0, h * rotateSpeed, 0);
+
+            transform.localPosition += velocity * Time.fixedDeltaTime;
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("floor"))//landing
+        {
+            onGround = true;
+            player.useGravity = true ;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("floor"))// taking off
+        {
+            
+            player.useGravity = false;
+            onGround = false;
+            player.transform.Translate(0, 0, 0.8f);
+            player.transform.Rotate(90, 0, 0);
+           // player.constraints
+        }
+               
     }
 }
