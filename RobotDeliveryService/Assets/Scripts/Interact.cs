e639b3938b201966fff5c86e56201f9885e9726e
mandy <lymandy97@gmail.com>
2019-02-16 12:05:01 -0700

    added elevator interact

---
+++ RobotDeliveryService/Assets/Scripts/Interact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public float _Distance;
+    public GameObject _CommandDisplay;
+    public GameObject _CommandText;
+    public GameObject _Player;
+    public GameObject _Building;
+    void Update()
+    {
+       
+        _Distance = PlayerCasting.Distancefromtarget;
+
+        if (_Distance <= 1)
+        {
+            _CommandDisplay.SetActive(true);
+            _CommandText.SetActive(true);
+        }
+        else
+        {
+            _CommandText.SetActive(false);
+            _CommandDisplay.SetActive(false);
+
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if (_Distance <= 1)
+            {
+                _CommandDisplay.SetActive(false);
+                _CommandText.SetActive(false);
+                _Player.transform.Translate(Vector3.up * 7);
+                _Player.transform.Translate(Vector3.forward * 1);
+            }
+        }
+    }
+        
+}
