56ff66fbf060d8015af8d216635bbc372c2d343e
Jason Xu <lemonade3532@outlook.com>
2019-02-16 22:13:34 -0800

    Energy HUD added to JasonHUDTest

---
+++ RobotDeliveryService/Assets/Scripts/PlayerToEnergyBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerToEnergyBar : MonoBehaviour
+{
+
+	public Image bar;
+	public Text energy;
+	private PlayerController pc;
+
+	private float ratioMaxEnergy;
+	public Color regularColor;
+	public Color redColor;
+
+    void Start()
+    {
+		pc = GetComponent<PlayerController>();
+		ratioMaxEnergy = 1 / (float) pc.MaxEnergy;
+
+		regularColor = energy.color;
+	}
+	
+    void Update()
+    {
+		bar.fillAmount = pc.Energy * ratioMaxEnergy;
+		if (pc.Energy <= 15)
+			energy.color = redColor;
+		else
+			energy.color = regularColor;
+		energy.text = pc.Energy.ToString();
+    }
+}
