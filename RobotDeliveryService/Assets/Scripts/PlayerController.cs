e7087412b369090d015f90a66273c4783155bad3
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-02-21 16:12:57 -0800

    Refactored PlayerController.cs & fixed one bug that causes a lag

--- RobotDeliveryService/Assets/Scripts/PlayerController.cs
+++ RobotDeliveryService/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,7 +11,7 @@ public class PlayerController : MonoBehaviour
 
     public Animator playerAnimator;
 
-    public  int Energy;
+    public int Energy;
     public int MaxEnergy;
     public float EnergyCostWalk; //units of energy per second
     public float EnergyCostGlide; //units of energy per second
@@ -26,14 +27,14 @@ public class PlayerController : MonoBehaviour
     public float accelration;
     public float fallingSpeed;
 
-    public bool isSuiside = false;
-    public bool isCrushed = false;
+    public bool isSuiciding = false;
+    public bool isCrashing = false;
 
     public Transform robot;
     public bool onGround = true;
 
     public GameObject youFell;
-    //public GameObject LevelAudio;
+    public GameObject LevelAudio;
     public GameObject fadeOut;
 
     // Start is called before the first frame update
@@ -42,117 +43,105 @@ public class PlayerController : MonoBehaviour
         player = GetComponent<Rigidbody>();
         initialRotation = player.rotation;
         Energy = MaxEnergy;
-        isSuiside = false;
-        isCrushed = false;
+        isSuiciding = false;
+        isCrashing = false;
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        Vector3 velocity; // on ground
-        Vector3 velocity2d; // flying planar velocity
-        Vector3 velocityVertical; // falling velocity
-        if (isSuiside) //jump backwards from a building
+        
+        if (isSuiciding) //jump backwards from a building
+            HandleSuicide();
+        else if (isCrashing) // crash onto a building during flight
+            HandleCrash();
+        else if (!onGround)// gliding
+            HandleGliding(h, v);
+        else // walking
+            HandleWalking(h, v);
+    }
+
+    private void HandleWalking(float h, float v)
+    {
+        Vector3 velocity = new Vector3(0, 0, groundSpeed * v);
+        if (v != 0) 
         {
-            onGround = false;
-            player.transform.Translate(new Vector3(0, fallingSpeed * 2 * Time.fixedDeltaTime, 0));
-            player.transform.Rotate(new Vector3(0, 1.5f, 0));
+            //only walking forward or backward will consume energy
+            EnergyTimer += Time.fixedDeltaTime;
+            if (EnergyTimer > 1 / EnergyCostWalk)
+            {
+                Energy--;
+                EnergyTimer = 0;
+            }
+            if (Energy <= 0)
+                StartCoroutine(YouFellOff());
         }
-        else if (isCrushed)
-        {
-            crash();
-        }
+
+        velocity = transform.TransformDirection(velocity);
+        transform.Rotate(0, h * rotateSpeed, 0);
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+        // Animation
+        if (velocity.Equals(Vector3.zero))
+            playerAnimator.SetBool("isMoving", false);
         else
+            playerAnimator.SetBool("isMoving", true);
+    }
+
+    private void HandleGliding(float h, float v)
+    {
+        Vector3 velocityPlanar = new Vector3(0, speed * v, 0); // flying planar velocity
+        Vector3 velocityVertical = new Vector3(0, -fallingSpeed, 0); // falling velocity
+    
+        EnergyTimer += Time.fixedDeltaTime;
+        if (EnergyTimer > 1f / EnergyCostGlide)
         {
-            if (!onGround)//gliding
-            {
-                // Energy
-                EnergyTimer += Time.fixedDeltaTime;// moving or not, gliding consumes energy
-                if (EnergyTimer > 1f / EnergyCostGlide)//glide long enough to lose energy
-                {
-                    Energy--;
-                    EnergyTimer = 0;
-                }
-
-                // Gliding speed
-                velocityVertical = new Vector3(0, -fallingSpeed, 0);
-                transform.localPosition += velocityVertical * Time.fixedDeltaTime;
-                velocity2d = new Vector3(0, speed * v, 0);
-                transform.Rotate(0, 0, -h * rotateSpeed);
-
-                if (v > 0)
-                {
-                    velocity2d = transform.TransformDirection(velocity2d);
-                    transform.localPosition += velocity2d * Time.fixedDeltaTime;
-                }
-
-                int currentRotationY = (int) Mathf.Round(robot.localRotation.eulerAngles.y);
-                if (h > 0)
-                {
-					robot.localRotation = Quaternion.Euler(0, -135, 0);
-                }
-                else if (h < 0)
-                {
-					robot.localRotation = Quaternion.Euler(0, -45, 0);
-                }
-                else
-                {
-                    robot.localRotation = Quaternion.Euler(0, -90, 0);
-                }
-
-            }
-
-            else
-            {
-                //moving forward and backward on the ground
-                velocity = new Vector3(0, 0, groundSpeed * v);
-                if (v != 0) //only walking forward or backward will consume energy
-                    EnergyTimer += Time.fixedDeltaTime;
-                {
-                    if (EnergyTimer > 1 / EnergyCostWalk)//walk long enough to lose energy
-                    {
-                        Energy--;
-                        EnergyTimer = 0;
-						if (Energy <= 0) {
-							StartCoroutine(YouFellOff());
-						}
-                    }
-                }
-                if (v < 0)
-                {
-                    Debug.Log("ReadEnergy");
-                    Debug.Log(Energy);
-                }
-
-                velocity = transform.TransformDirection(velocity);
-                // turning on the ground
-                transform.Rotate(0, h * rotateSpeed, 0);
-
-                transform.localPosition += velocity * Time.fixedDeltaTime;
-
-                if (velocity.Equals(Vector3.zero))
-                    playerAnimator.SetBool("isMoving", false);
-                else
-                    playerAnimator.SetBool("isMoving", true);
-            }
-
+            Energy--;
+            EnergyTimer = 0;
         }
 
+        transform.localPosition += velocityVertical * Time.fixedDeltaTime;
+        transform.Rotate(0, 0, -h * rotateSpeed);
+
+        if (v > 0)
+        {
+            velocityPlanar = transform.TransformDirection(velocityPlanar);
+            transform.localPosition += velocityPlanar * Time.fixedDeltaTime;
+        }
+
+        int currentRotationY = (int)Mathf.Round(robot.localRotation.eulerAngles.y);
+        if (h > 0)
+            robot.localRotation = Quaternion.Euler(0, -135, 0);
+        else if (h < 0)
+            robot.localRotation = Quaternion.Euler(0, -45, 0);
+        else
+            robot.localRotation = Quaternion.Euler(0, -90, 0);
+    }
+
+    private void HandleSuicide()
+    {
+        onGround = false;
+        player.transform.Translate(new Vector3(0, fallingSpeed * 2 * Time.fixedDeltaTime, 0));
+        player.transform.Rotate(new Vector3(0, 1.5f, 0));
+    }
+
+    private void HandleCrash()
+    {
+        player.transform.Translate(new Vector3(0, 0, fallingSpeed * 4 * Time.fixedDeltaTime));
+        player.transform.Rotate(new Vector3(0, 0, 3.5f));
+        //Todo
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision);
-        Debug.Log(collision.gameObject.tag.Equals("floor"));
-        if (!onGround)//flying, and dying
+        if (!onGround)//flying or upon a crash
         {
-            if (!isSuiside)//flying for sure
+            if (!isSuiciding)
             {
                 if (collision.gameObject.tag.Equals("Building"))//crash on the building
                 {
-                    
-                    isCrushed = true;
+                    isCrashing = true;
                     Energy -= CrashDamage;
                     robot.localRotation = Quaternion.Euler(0, -90, 0);
                     player.transform.Translate(0, -1.8f, 0);
@@ -163,18 +152,17 @@ public class PlayerController : MonoBehaviour
 
         if (collision.gameObject.tag.Equals("floor") && !onGround)//landing
         {
-            if (isCrushed)
-            {
-                isCrushed = false;
-            }
-            if (isSuiside)
+            if (isCrashing)
+                isCrashing = false;
+
+            if (isSuiciding)
             {
                 Energy = 0;
-                isSuiside = false;
+                isSuiciding = false;
                 StartCoroutine(YouFellOff());
             }
-            EnergyTimer = 0;//grace reset on landing and taking off
-            Debug.Log("oncollision ");
+
+            EnergyTimer = 0;
             onGround = true;
             player.useGravity = true;
             player.transform.Rotate(-90, 0, 0);
@@ -192,51 +180,41 @@ public class PlayerController : MonoBehaviour
         youFell.SetActive(true);
 		Time.timeScale = 0.3f;
         yield return new WaitForSecondsRealtime(2);
-		//LevelAudio.SetActive(false);
+		LevelAudio.SetActive(false);
 		fadeOut.SetActive(true);
         yield return new WaitForSecondsRealtime(1);
         SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Roof"))// taking off
+        // taking off from a roof
+        if (collision.gameObject.tag.Equals("Roof"))
         {
             if (Input.GetAxis("Vertical") < 0)
             {
                 player.transform.Translate(0, 0, -1.8f);
                 player.transform.Rotate(180, 0, 0);
 
-                isSuiside = true;
-                playerAnimator.SetBool("isFlying", true);
+                isSuiciding = true;
 
 				PlayerGlidingPPChange pp = GetComponent<PlayerGlidingPPChange>();
 				pp.SetIsFlying(true);
 			}
             else
             {
-                EnergyTimer = 0;//grace reset on landing and taking off
+                EnergyTimer = 0;
                 player.useGravity = false;
                 onGround = false;
                 player.transform.Translate(0, 0, 0.8f);
                 player.transform.Rotate(90, 0, 0);
-                // player.constraints
 
                 playerAnimator.SetBool("isFlying", true);
 
 				PlayerGlidingPPChange pp = GetComponent<PlayerGlidingPPChange>();
 				pp.SetIsFlying(true);
 			}
-
-
         }
-
-    }
-
-    private void crash()
-    {
-        player.transform.Translate(new Vector3(0,0, fallingSpeed * 4 * Time.fixedDeltaTime));
-        player.transform.Rotate(new Vector3(0,0 ,3.5f));
-        //Todo
     }
 }
